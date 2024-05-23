74874a9a37b839a617cae2db024ec068853cf14f
makkksesh <maksimka2388@gmail.com>
2024-05-23 16:32:59 +0300

    new class

--- For Git.cpp
+++ For Git.cpp
@@ -5,5 +5,6 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
     cout << "Hello World!\n";
+	cout << "Привет Мир!\n";
 }
 
---
+++ Person.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class Person
+{
+	int birthDate;
+	string name;
+public:
+
+};
+
